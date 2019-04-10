d278000990f3a5e2afa55c54b95170cc273d92cf
bego <begomontes7@gmail.com>
2019-04-09 20:41:32 -0500

    Fixed broken footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,95 +1,86 @@
+
+
 <footer class="page-footer font-small " id="footer" >
-      <div class="feature-wrapper p-3  bg-light text-dark">
-          <div class="container">
-              <div class="row justify-content-around">
+<div class="feature-wrapper p-3  bg-light text-dark">
+    <div style="clear:both; margin:100px" > </div>
 
-                  <div class="col-sm-12 col-md-3 text-center text-md-left ">
-                      <h4 class="text-uppercase">CONTACTO</h4>
-                  </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-3 text-center text-md-left  mb-3 mb-md-0">
+                <h4 class="text-uppercase" >CONTACTO</h4>
+                <br>
+                <br>
 
-                  <div class="col-sm-12 col-md-3">
-                    <h4 class="text-uppercase">ENLACES RELACIONADOS</h4>
-                  </div>
-
-                  <div class="col-sm-12 col-md-3">
-                    <h4 class="text-uppercase">NOSOTROS</h4>
-                  </div>
-
-              </div> 
-
-              <div class="row justify-content-around">
-                     <div class="col-sm-12 col-md-3">
-                          <h6>+222 838 36 91</h6>
-                          <h6>contacto@portalsocial.mx</h6>
-                     </div>
-
-                     <div class="col-sm-12 col-md-3">
-                        <ul class="list-unstyled">
-                             <li>
-                                <a href="https://www.youtube.com/channel/UC4XjsjerjcQg2vIH-4POyYA" target="_blank">•  Nuestro canal </a>
-                             </li>
-
-                             <li>
-                                <a href="http://portalsocial.mx/" target="_blank"> •  www.portalsocial.mx</a>
-                             </li>
-
-                             <li>
-                                 <a href="http://www.thetreeschool.org/" target="_blank">•  www.thetreeschool.org </a>
-                             </li>
-
-                             <li>
-                                  <a href="http://portalsocial.mx/beneficios/" target="_blank">•   Beneficios de los árboles</a>
-                             </li>    
-                        </ul>
-                     </div>
-
-                     <div class="col-sm-12 col-md-3">
-                            <h6>Dona un Árbol al Mundo es una campaña de restauración ambiental y empoderamiento social que busca recuperar nuestros bosques y ayudar a 
-                            comunidades con rezago social.</h6>
-                     </div>
-              </div> 
-
-              <div class="row justify-content-start">
-                  <div class="col-sm-12 col-md-12">
-                      <p>Búscanos en nuestras redes sociales</p>
-                  </div>
-              </div>
-
-                  <div class="row justify-content-around">
-                      <div class="col-sm-12 col-md-12">
-                          <a href="https://www.facebook.com/TheTreeSchool/" target="_blank">
-                          <img class="alignleft wp-image-311" src="<?php echo base_url();?>/img/fb.png" alt="" width="60" height="60" /></a> 
-                          <a href="https://www.youtube.com/channel/UC4XjsjerjcQg2vIH-4POyYA" target="_blank">
-                          <img class="alignleft wp-image-313" src="<?php echo base_url();?>/img/yout.png" alt="" width="85" height="83" /></a>
-                      </div>
-                  </div>
-
-                  <div class="row justify-content-around">
-                      <div class="col-sm-12 col-md-12">
-                           <a> Dona un Árbol al Mundo ® Todos los derechos reservados</a>
-                      </div>
-                  </div>  
-          </div>
-      </div>
-</footer>
-
- 
+ <h6>+222 838 36 91 
+   <h3></h3> </h6>
+<h6>contacto@portalsocial.mx</h6>
+<p>&nbsp;</p>
+<br>
+<br>
+<p>Búscanos en nuestras redes sociales</p>
+<!-- <p>&nbsp;</p><p>&nbsp;</p><p>&nbsp;</p> -->
 
 
 
 
+<p><a href="https://www.facebook.com/TheTreeSchool/" target="_blank">
+  <img class="alignleft wp-image-311" src="<?php echo base_url();?>/img/fb.png" alt="" width="60" height="60" /></a> 
+   &nbsp;
+  <a href="https://www.youtube.com/channel/UC4XjsjerjcQg2vIH-4POyYA" target="_blank">
+    <img class="alignleft wp-image-313" src="<?php echo base_url();?>/img/yout.png" alt="" width="85" height="83" /></a>
+</p>
+            </div>
+           <div class="col-sm-12 col-md-1 text-center mb-3 mb-md-0">
+                
+            </div>
+            <div class="col-sm-12 col-md-3 text-center text-md-left  mb-3 mb-md-0">
+                <h4 class="text-uppercase">ENLACES RELACIONADOS</h4>
+                <br>
+                <br>
 
+            <ul class="list-unstyled">
+            <li><h6>
+                <a href="https://www.youtube.com/channel/UC4XjsjerjcQg2vIH-4POyYA" target="_blank">•  Nuestro canal <p>&nbsp;</p></a></h6>
+              </li>
+              <li><h6>
+  
+                <a href="http://portalsocial.mx/" target="_blank"> •  www.portalsocial.mx <p>&nbsp;</p></a></h6>
+
+              </li>
+              <li><h6>
+                <a href="http://www.thetreeschool.org/" target="_blank">•  www.thetreeschool.org <p>&nbsp;</p></a></h6>
+              </li>
+              <li>
+               <h6><a href="http://portalsocial.mx/beneficios/" target="_blank">•   Beneficios de los árboles</a></h6>
+              </li>
+              
+            </ul>
+
+            </div>
+            <div class="col-sm-12 col-md-1 text-center mb-3 mb-md-0">
+               
+            </div>
+            <div class="col-sm-12 col-md-3 text-center text-md-left  mb-3 mb-md-0">
+                 <h4 class="text-uppercase">NOSOTROS</h4>
+                 <br>
+                 <br>
+          <div class=""> <h6>Dona un Árbol al Mundo es una campaña de restauración ambiental y empoderamiento social que busca recuperar nuestros bosques y ayudar a 
+          comunidades con rezago social.
+</h6>
             
-<!-- </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="container">
     <div class="row">
         <div class="col-sm-12 col-md-5 text-center text-md-left  mb-3 mb-md-0">
-            
+            <a> Dona un Árbol al Mundo ® Todos los derechos reservados</a>
         </div>
     </div>
-</div> -->
+</div>
 
 
-
-
+ 
+</footer>
