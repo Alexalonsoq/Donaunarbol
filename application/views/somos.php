e8e41dd6b7f359cd28c79ef91faf476b5ad0f20f
bego <begomontes7@gmail.com>
2019-04-24 19:42:11 -0500

    Added view carlos

--- application/views/somos.php
+++ application/views/somos.php
@@ -10,33 +10,27 @@
     transition: 0.3s;
     margin-bottom:10px;
 }
-
 .price:hover {
     box-shadow: 0 8px 12px 0 rgba(0,0,0,0.2)
 }
-
 .price .header {
     background-color: #111;
     color: white;
     font-size: 25px;
 }
-
 .price li {
     border-bottom: 1px solid #eee;
     padding: 20px;
     text-align: center;
 }
-
 .price .grey {
     background-color: #eee;
     font-size: 20px;
 }
-
 .price .green {
     background-color: #8db600;
     font-size: 20px;
 } 
-
 .price .blue {
     background-color: #6495ED;
     font-size: 20px;
@@ -50,44 +44,30 @@
     text-decoration: none;
     font-size: 18px;
 }
-
 div.a {
   text-align: center;
 }
-
 div.b {
   text-align: left;
 }
-
 div.c {
   text-align: right;
 }
-
 div.d {
   text-align: justify;
 }
-
 #bienvenida {color:white;background:#B7B4D5;}
 #bienvenida2 {color:#0f0;}
 #bienvenida3 {color:#f00;}
-
-
-
 .graph {
 position: relative; /* IE is dumb */
 width: 500px;
-
 padding: 2px;
 }
-
 .right{
     float: right;
     background:red
 }
-
-
-
-
 .graph .bar {
 display: block;
 position: relative;
@@ -98,120 +78,144 @@ height: 2em;
 line-height: 2em;
 }
 .graph .bar span { position: absolute; left: 1em; }
-
-
 @media only screen and (max-width: 600px) {
     .columns {
         width: 100%;
     }
 }
-
 </style>
 
 
 <div class="row">
   <div class="col-sm-6 "><img src="../img/frase.jpg"  class="img-responsive"></div>
   
-  <div class="col-sm-6 ">
+  <div class="col-sm-6 align-self-center" >
 
-   <div class="container-fluid ">
-    <P> <h4>Esta campaña tiene muchos colaboradores, gente incansable que 
-    hace la diferencia día a día, personas que piensan que trabajar
-    por la mejora del mundo no es tiempo perdido, por el contrarío,
-    creen que es una forma de corresponder al planeta pues nos ha
-    dado todo sin condiciones. Algunos de ellos entregan su trabajo
-    otros entregan su donación pero todos contribuyen. </h4></P> 
+    <p class="card-text" style="color: black; padding-left: 10%;">
+    Esta campaña es dirigida por el Portal Social y The Tree School, quienes trabajan<br>
+    de la mano de muchas comunidades que se encuentran en las Reservas<br>
+    de la Biosfera de Querétaro y Puebla en México, así como en Kakataima,<br>
+     Quindío, Colombia.</p> 
    
     <br> 
-    <h4> Entonces partiendo del hecho de que esta campaña la construyen miles de personas podemos decir que es guíada por tres organismos: El Portal Social, The Tree School y el vivero Rio Blanco.
-      </h4> 
-     </div>
+    <p class="card-text" style="color: black; padding-left: 10%;">
+     Además tiene muchos colaboradores, gente incansable que hace la diferencia<br>
+    día con día, personas que piensan que trabajar por la mejora del mundo no es <br>
+    tiempo perdido, por el contrario, creen que es una forma de corresponder al<br>
+    planeta por todo lo que nos ha dado, Algunos de ellos entregan su trabajo<br>
+    otros entregan su donación pero todos contribuyen.
+   </p> 
+    
   </div>
 
 </div>
 
 
 <div class="row">
-  <div class="col-sm-6 container-fluid"><img src="../img/portal.jpg"  class="img-responsive"></div>
+  <div class="col-sm-6" style="padding: 0px;">
+    <img src="../img/portal.jpg" class="img-responsive">
+    </div>
   
-  <div class="col-sm-6 container-fluid">
-
-   <div class="container-fluid" >
-       <h3> Portal social</h3>
+  <div class="col-sm-6" align="left" style="padding-left: 5%;background-color: rgb(244, 244, 244)" >
+       <h2 style="margin-top: 10%"><strong> PORTAL SOCIAL</strong></h2>
        <br><br>
 
-     <h4>Es una plataforma digital que apoya a profesionales del área social
-    así como a ONG's de diversas ramas. Tiene la tarea de junto con
-    ellos ofrecer contenido audiovisual con sentido social. Esto lo hace
-    por medio de vídeos, infografías, blog, etc. Difundiendo contenidos
-    educativos, informativos y motivacionales con el objeto de incidir en
-    los usuarios de manera positiva para que puedan conocer a fondo
-    problemáticas de índole social, tomar consciencia y participar para
-    su solución. </h4>
+       <p class="card-text" style="color: black; margin-top: 5%">
+       Es una plataforma digital que apoya a profesionales del área social<br>
+    así como a ONG's de diversas ramas. Tiene la tarea de junto con<br>
+    ellos ofrecer contenido audiovisual con sentido social. Esto lo hace<br>
+    por medio de vídeos, infografías, blog, etc. Difundiendo contenidos<br>
+    educativos, informativos y motivacionales con el objeto de incidir en<br>
+    los usuarios de manera positiva para que puedan conocer a fondo<br>
+    problemáticas de índole social, tomar consciencia y participar para<br>
+    su solución. </p>
   
    
-    <br> <h2><a href="https://portalsocial.mx/" target="_blank">www.portalsocial.mx 
+    <br> <br><br><h2><a style="color: black; margin-top: 5%" href="https://portalsocial.mx/" target="_blank">www.portalsocial.mx 
       </a> </h2>
-     
-
+      </div>
      </div>
   </div>
-
-</div>
 </div>
 
-<div style="clear:both; margin:10px" > </div>
- 
 
 <div class="row">
-  <div class="col-sm-6"><br>
-
-   <div class="container-fluid" >
-       <h3> THE TREE SCHOOL</h3>
+  <div class="col-sm-6" align="right" style="padding-right: 5%;background-color: rgb(133, 133, 133);color: white">
+       <h2 style="margin-top: 10%"><strong> THE TREE SCHOOL</strong></h2>
        <br><br>
-
-     <h4>Es una asociación civil comprometida con la Educacion para un Desarrollo sustentable, integrada por un equipo multidiciplinario de expertos(as), que trabajancon honestidad, respeto y pasion, ofresiendosoluciones integrales que incrementen el bienestar economico, mejoren las relaciones sociales en armonía con el ambiente, para satisfacer las necesidades del presente sin comprometer las del futuro</h4>
-  
-    <br> <h2> <a href="http://www.thetreeschool.org/" target="_blank">www.thetreeschool.org 
+    <p class="card-text" style="color: white; margin-top: 5%">
+     Es una asociación civil comprometida con el Desarrollo Sostenible.<br>
+    Dedicada a generar soluciones integrales para que el medio ambiente<br>
+     se respete, preserve, restaure y al mismo tiempo se incremente el<br>
+    bienestar económico y social. Su misión primordial es que las personas<br>
+     tomen conciencia de los problemas globales y participen para mejorar<br>
+    las condiciones de las presentes y futuras generaciones.
+    </p>
+    <br><br><br> <h2> <a style="color: white;" href="http://www.thetreeschool.org/" target="_blank">www.thetreeschool.org 
       </a></h2>
-     </div>
   </div>
 
-  <div class="col-sm-6 "><img src="../img/thetree.jpg"  class="img-responsive"></div>
-  
+  <div class="col-sm-6" style="padding: 0px;">
+    <img src="../img/thetree.jpg"  class="img-responsive">
   </div>
-
-
-<div style="clear:both; margin:10px" > </div>
- 
-
-<div class="row">
-
-  <div class="col-md-6 container-fluid"><img src="../img/rio.jpg" width="655" height="700" class="img-responsive"></div>
-  
-
-  <div class="col-md-6 container-fluid"><br>
-
-   <div class="container-fluid" >
-       <h3> VÍVERO RÍO BLANCO</h3>
-       <br><br>
-
-     <h4>Es un vívero enclavado en la Sierra Gorda de Quer'etaro, tiene el objeto de reforestar, conservar y proveer recursos económicos y ambientales a su comunidad, Su capacidad esde 80 mil plantas al año.</h4>
-     <h4>Es muy valioso resaltar que este proyecto ha sido liderado por mujeres desde su inicio.</h4>
-  
-    
-     </div>
-  </div>
-
-  
   
   </div>
 
 
-  
+
+<div class="row justify-content-center" style="margin-top: 30px">
+  <div class="col-3">
+    <h2><strong>Reservas y Comunidades</strong></h2>
+  </div>
+  <div class="col-3"></div>
+  <div class="col-3"></div>
+</div>
+<div class="row justify-content-center" style="margin-top: 20px">
+  <div class="col-sm-12 col-md-3">
+    <div class="card float-right" style="width: 100%; border: none;">
+      <img src="../MaterialPaul/amazing-austria-dawn-1323550.jpg" class="card-img-top" width="10" height="200">
+      <div class="card-body">
+        <h4 class="card-title"><strong>Reserva de la Biosfera de la Sierra<br> Gorda Querétaro México</strong></h4>
+        <p class="card-text">
+        Esta reserva cuenta con una extensión de 383 567
+        hectáreas en las cuales encontramos bosque tropical subcaducifolio,
+        bosque tropical caducifolio, matorral xerófilo, encinar arbustivo, pastizal, bosque de Quercus, bosque de 
+        coníferas y bosque mesófilo de montaña. En este lugar trabajamos con las comunidades de Río Blanco, Alejandría,
+        Agua Zaca y Valle Verde.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-12 col-md-3">
+    <div class="card float-center" style="width: 100%; border: none;">
+    <img src="../MaterialPaul/backlit-blurred-background-desert-1222561.jpg" class="card-img-top" width="10" height="200">
+      <div class="card-body">
+        <h4 class="card-title"><strong>Reserva de la Biosfera<br> Tehuacán-Cuicatlán México</strong></h4>
+        <p class="card-text">
+        Esta reserva cuenta con una extensión de 490 187 hectáreas en las cuales encontramos bosque
+        tropical caducifolio, bosque espinoso, bosque de encino, pastizal y matorral xerófilo. En este lugar
+        trabajamos con comunidades de Tehuacán, Cuapán y Santa Ana Teñpxtoc.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-12 col-md-3">
+    <div class="card" style="width: 100%; border: none;">
+    <img src="../MaterialPaul/bright-daylight-environment-396738.jpg" class="card-img-top" width="10" height="200">
+      <div class="card-body">
+        <h4 class="card-title"><strong>Córdoba y Calarcá, Quindío, Colombia</strong></h4>
+        <p class="card-text">
+        En esta zona llena de riquezas naturales y humanas, trabajamos con el Centro para el Fortalecimiento
+        Campesino Kakataima.    
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+  </div>
+</div>
  
 
 	<?php 
 include('footer.php');
-
 ?>
